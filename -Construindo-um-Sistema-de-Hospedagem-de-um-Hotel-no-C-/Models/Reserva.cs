a79cbdb803c328deae5e24c8e6c9a2c1d4e3c3b5
Paulo Tuppy <130563312+TuppyPower@users.noreply.github.com>
2023-09-26 10:17:23 -0300

    Add files via upload

---
+++ -Construindo-um-Sistema-de-Hospedagem-de-um-Hotel-no-C-/Models/Reserva.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // Lançar uma exceção caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("A capacidade da suite é insuficiente para o número de hóspedes.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes != null ? Hospedes.Count : 0;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor -= valor * 0.10m; // Aplica o desconto de 10%
+            }
+
+            return valor;
+        }
+    }
+}
